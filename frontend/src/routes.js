dcdb44aa62dd2891327a8ba1bb74bf042eefd5ef
Mozhgan <mojganshah@gmail.com>
2024-12-06 22:56:40 +0330

    routes

---
+++ frontend/src/routes.js
@@ -0,0 +1,11 @@
+import Index from "./pages/Index/Index";
+import CourseInfo from "./pages/CourseInfo/CourseInfo";
+
+const routes = [
+  {
+    path: "/",
+    element: <Index />,
+  },
+];
+
+export default routes;
