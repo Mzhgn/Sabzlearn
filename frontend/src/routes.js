05032aec09bed631621c7f92f150bc6b667e4b91
Mozhgan <mojganshah@gmail.com>
2024-12-07 22:49:55 +0330

    creating category file and path

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,6 @@
 import Index from "./pages/Index/Index";
 import CourseInfo from "./pages/CourseInfo/CourseInfo";
+import Category from "./pages/Category/Category";
 
 const routes = [
   {
@@ -7,7 +8,7 @@ const routes = [
     element: <Index />,
   },
   { path: "/course-info/:courseName", element: <CourseInfo /> },
-  {},
+  { path: "/category-info/:categoryName", element: <Category /> },
 ];
 
 export default routes;
