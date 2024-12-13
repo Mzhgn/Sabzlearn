dc2ca62cb502857a6854c4cce4866e173df24a86
Mozhgan <mojganshah@gmail.com>
2024-12-13 23:22:34 +0330

    adding use routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import routes from "./routes";
 
-import './App.css'
+import "./App.css";
 
 export default function App() {
   return (
     <div>
       <h1>Sabzlearn.ir</h1>
     </div>
-  )
+  );
 }
